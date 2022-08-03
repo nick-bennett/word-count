2f64c9192f42baf1c52c5eaddc72d6a4a36ed06e
Nick Bennett <nick@nickbenn.com>
2022-08-03 14:40:17 -0600

    Started creating unit tests.

--- src/main/java/com/tlglearning/wordcount/WordCounter.java
+++ src/main/java/com/tlglearning/wordcount/WordCounter.java
@@ -1,11 +1,16 @@
 package com.tlglearning.wordcount;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class WordCounter {
+
+  private static final Set<String> BORING_WORDS = Set.of("and", "of", "the", "in", "on", "i", "then", "than", "out", "a", "if");
 
   private final Map<String, Integer> counts = new HashMap<>();
 
@@ -51,10 +56,13 @@ public class WordCounter {
   }
 
   void countWords(String[] words) {
-    for (String word : words) {
-      counts.put(word, get(word) + 1);
-      totalWords++;
-    }
+    Arrays
+        .stream(words)
+        .map(String::trim)
+        .filter((word) -> !word.isEmpty())
+        .filter((word) -> word.length() > 5)
+//        .filter(Predicate.not(String::isEmpty))
+        .forEach((word) -> counts.put(word, 1 + counts.getOrDefault(word, 0)));
   }
 
 }
